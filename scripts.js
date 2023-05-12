354e722e7bafa2df01a7e4c545e0dfc286af17a6
FodorIustin <iustin.fodor@emanuel.ro>
2023-05-12 08:34:03 +0300

    setup-creare de input-uri

--- scripts.js
+++ scripts.js
@@ -42,7 +42,14 @@ afisare(xInit, y) {
     y += cubeSize;
   }
 }
-
+reset() {
+  //reseteaza patratul
+  for (let r = 0; r < length; r++)
+    for (let c = 0; c < length; c++) {
+      this.patrat[r][c].color = "white";
+      this.patrat[r][c].value = "";
+    }
+}
 clicked(my, mx) {
   //verifica daca am dat click pe patrat
   for (let r = 0; r < length; r++)
@@ -54,4 +61,30 @@ clicked(my, mx) {
         my <= 20 + cubeSize + cubeSize * c
       )
         this.patrat[r][c].color = "green";
+}
+
+function setup() {
+	//functie care initializeaza patratul
+	createCanvas(canvasWidth, canvasHeight);
+	patrat.initializare();
+	resetbt = createButton("RESET");
+	resetbt.position(440, 20);
+	resetbt.size(60, 25);
+	resetbt.mousePressed(reset);
+	resetbt.style("background-color", "lightgrey");
+
+	playerInput2 = createInput();
+	playerInput2.position(400, 380);
+
+	let adddButton = createButton("Add name");
+	adddButton.position(400, 410);
+	adddButton.mousePressed(addName2);
+
+	playerInput = createInput();
+	playerInput.position(20, 380);
+
+	let addButton = createButton("Add name");
+	addButton.position(20, 410);
+	addButton.mousePressed(addName);
+
 }
