86a7cfad7a5d33a335e846f0abac6c58032a211a
FodorIustin <iustin.fodor@emanuel.ro>
2023-05-12 08:32:38 +0300

    Afisare+CLicked

--- scripts.js
+++ scripts.js
@@ -27,3 +27,31 @@ class Patrat {
     }
   }
 }
+afisare(xInit, y) {
+  //afiseaza patratul
+  let x = xInit;
+  for (var r = 0; r < length; r++) {
+    x = xInit;
+    for (var c = 0; c < length; c++) {
+      stroke("black");
+      strokeWeight(3);
+      fill(this.patrat[r][c].color);
+      rect(x, y, cubeSize, cubeSize);
+      x += cubeSize;
+    }
+    y += cubeSize;
+  }
+}
+
+clicked(my, mx) {
+  //verifica daca am dat click pe patrat
+  for (let r = 0; r < length; r++)
+    for (let c = 0; c < length; c++)
+      if (
+        mx >= 20 + cubeSize * r &&
+        mx <= 20 + cubeSize + cubeSize * r &&
+        my >= 20 + cubeSize * c &&
+        my <= 20 + cubeSize + cubeSize * c
+      )
+        this.patrat[r][c].color = "green";
+}
